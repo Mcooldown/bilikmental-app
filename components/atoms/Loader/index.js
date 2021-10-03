cffed80eacf125686bbdd15152b1626ed2968b66
Mcooldown <vincenthadinata30@gmail.com>
2021-10-03 12:56:59 +0700

    clas to classname

--- components/atoms/Loader/index.js
+++ components/atoms/Loader/index.js
@@ -2,7 +2,7 @@ import style from "./Loader.module.css";
 
 const Loader = () => {
      return (
-          <div class=" flex justify-center items-center">
+          <div className=" flex justify-center items-center">
                <div className={ style.loader +  " ease-linear rounded-full border-8 border-t-8 border-gray-200 h-16 w-16"}></div>
           </div>  
      )
